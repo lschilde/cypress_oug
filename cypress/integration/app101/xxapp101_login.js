7d8a46c91f5a6939f6cc8ef8baef809d4a1e8246
lschilde <lschilde@gmail.com>
2022-05-19 10:18:47 +0200

    Update xxapp101_login.js

--- cypress/integration/app101/xxapp101_login.js
+++ cypress/integration/app101/xxapp101_login.js
@@ -1,3 +1,9 @@
+Cypress.on('uncaught:exception', (err, runnable) => {
+  // returning false here prevents Cypress from
+  // failing the test
+  return false
+  });
+
 describe('First APP demo', () => {
   it('Open login page', () => {
     cy.visit('https://icona8dnofzzzzp-db202010161259.adb.ap-sydney-1.oraclecloudapps.com/ords/r/lino/101/login_desktop');
