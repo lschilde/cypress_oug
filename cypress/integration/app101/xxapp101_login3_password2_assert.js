b9d42db8c044336b26a1dd460e6ae12ce57bc91c
lschilde <lschilde@gmail.com>
2022-05-19 10:20:06 +0200

    Update xxapp101_login3_password2_assert.js

--- cypress/integration/app101/xxapp101_login3_password2_assert.js
+++ cypress/integration/app101/xxapp101_login3_password2_assert.js
@@ -1,3 +1,9 @@
+Cypress.on('uncaught:exception', (err, runnable) => {
+  // returning false here prevents Cypress from
+  // failing the test
+  return false
+  });
+
 describe('App 110 open test with baseURL', () => {
     it('Opening login page', () => {
         //using baseURL variable
